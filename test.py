e596f3a62a6eb5112ceccd88dedd136f67adeca3
Greg Konradt <greg.konradt@datorama.com>
2017-05-14 11:03:27 +0300

    Catching errors and introducing a loop

--- test.py
+++ test.py
@@ -12,22 +12,22 @@ def getAudioInput():
         print("I'm listening...")
         audio = r.listen(source)
 
-
+    global audioInput
+    audioInput = ""
     # recognize speech using Google Speech Recognition
     try:
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         #print("Google Speech Recognition thinks you said: " + r.recognize_google(audio))
-        print("")
+        print("")        
+        audioInput = r.recognize_google(audio).lower()
+
+        print("This is the output of getAudioInput: " + audioInput)
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        print("Google Speech Recognition could not understand the audio input.")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    #   
-    global audioInput
-    audioInput = r.recognize_google(audio).lower()
-    print("This is the output of getAudioInput: " + audioInput)
     return audioInput    
 
 def playVideo(filename):
@@ -62,11 +62,12 @@ def determineVideoFilename(userInput):
 #subprocess.call('open ' + vid1, shell=True)
 #playVideo("./play.mov")
 ####End of TESTING
-toDo = determineAction(getAudioInput()) 
+while True:
+    toDo = determineAction(getAudioInput()) 
 
-if  toDo == "repeatUserInput":
-    repeatUserInput(audioInput)
+    if  toDo == "repeatUserInput":
+        repeatUserInput(audioInput)
 
-elif toDo == "playVideo":
-    print("I will play a video called " + determineVideoFilename(audioInput))
-    playVideo(determineVideoFilename(audioInput))
+    elif toDo == "playVideo":
+        print("I will play a video called " + determineVideoFilename(audioInput))
+        playVideo(determineVideoFilename(audioInput))
